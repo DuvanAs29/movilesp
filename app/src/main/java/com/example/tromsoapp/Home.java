4e400246268b853e0b43e1a2b5897ec02d38d8de
donceya2093 <tajual93@gmail.com>
2023-07-26 19:01:49 -0500

    melo pero le falta

--- app/src/main/java/com/example/tromsoapp/Home.java
+++ app/src/main/java/com/example/tromsoapp/Home.java
@@ -3,6 +3,7 @@ package com.example.tromsoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +12,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.example.tromsoapp.R;
+
+import java.util.Locale;
 
 public class Home extends AppCompatActivity {
 
@@ -63,6 +66,22 @@ public class Home extends AppCompatActivity {
         });
     }
 
+
+    public void cambiarIdioma(String idioma){
+
+        //configurar el lenguaje del telefono
+        Locale lenguaje=new Locale(idioma);
+        Locale.setDefault(lenguaje);
+
+        //Configuramos globalmente el telefono
+        Configuration configuracionTelefono=getResources().getConfiguration();
+        configuracionTelefono.locale=lenguaje;
+
+        //Ejecutamos la configuracion
+        getBaseContext().getResources().updateConfiguration(configuracionTelefono,getBaseContext().getResources().getDisplayMetrics());
+
+    }
+
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu,menu);
         return true;
@@ -73,25 +92,31 @@ public class Home extends AppCompatActivity {
         switch (itemSeleccionado){
 
             case(R.id.opcion1):
-
+                this.cambiarIdioma("en");
+                Intent intentIngles=new Intent(Home.this,Home.class);
+                startActivity(intentIngles);
                 break;
             case(R.id.opcion2):
-
+                this.cambiarIdioma("es");
+                Intent intentEspanol=new Intent(Home.this,Home.class);
+                startActivity(intentEspanol);
                 break;
             case(R.id.opcion3):
-
+                this.cambiarIdioma("it");
+                Intent intentItaliano=new Intent(Home.this,Home.class);
+                startActivity(intentItaliano);
                 break;
             case(R.id.opcion4):
                 Intent intent = new Intent(Home.this,Acerca.class);
-
+                startActivity(intent);
                 break;
 
         }
+
         return super.onOptionsItemSelected(item);
     }
 
-    public void cambiarIdioma(String idioma){
 
-    }
+
 
 }
