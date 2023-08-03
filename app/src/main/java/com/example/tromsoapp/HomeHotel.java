c7c374a6a77771699aac22f4fe747389ce095290
tiago0625 <sospina0625@outlook.com>
2023-08-02 20:50:47 -0500

    se creo molde

---
+++ app/src/main/java/com/example/tromsoapp/HomeHotel.java
@@ -0,0 +1,14 @@
+package com.example.tromsoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeHotel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_hotel);
+    }
+}
---
+++ app/src/main/java/com/example/tromsoapp/moldes/MoldeHotel.java
@@ -0,0 +1,50 @@
+package com.example.tromsoapp.moldes;
+
+public class MoldeHotel {
+  private   String nombre1;
+  private String precio;
+  private String telefono;
+  private Integer foto;
+
+    public MoldeHotel() {
+    }
+
+    public MoldeHotel(String nombre1, String precio, String telefono, Integer foto) {
+        this.nombre1 = nombre1;
+        this.precio = precio;
+        this.telefono = telefono;
+        this.foto = foto;
+    }
+
+    public String getNombre1() {
+        return nombre1;
+    }
+
+    public void setNombre1(String nombre1) {
+        this.nombre1 = nombre1;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Integer getFoto() {
+        return foto;
+    }
+
+    public void setFoto(Integer foto) {
+        this.foto = foto;
+    }
+}
