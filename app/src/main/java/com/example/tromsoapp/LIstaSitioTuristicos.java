2bc430925b0337ddf1f283bc382ec3b17b102898
309 <309@academicos.cesde.edu.co>
2023-08-08 21:01:07 -0500

    se crea moldes

---
+++ app/src/main/java/com/example/tromsoapp/LIstaSitioTuristicos.java
@@ -0,0 +1,14 @@
+package com.example.tromsoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LIstaSitioTuristicos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_sitio_turisticos);
+    }
+}
---
+++ app/src/main/java/com/example/tromsoapp/moldes/MoldeRestaurante.java
@@ -0,0 +1,61 @@
+package com.example.tromsoapp.moldes;
+
+public class MoldeRestaurante {
+
+    private String nombreRes;
+    private Integer fotoRes;
+    private String rango;
+    private String telefonoRes;
+    private String recomendado;
+
+    public MoldeRestaurante() {
+    }
+
+    public MoldeRestaurante(String nombreRes, Integer fotoRes, String rango, String telefonoRes, String recomendado) {
+        this.nombreRes = nombreRes;
+        this.fotoRes = fotoRes;
+        this.rango = rango;
+        this.telefonoRes = telefonoRes;
+        this.recomendado = recomendado;
+    }
+
+    public String getNombreRes() {
+        return nombreRes;
+    }
+
+    public void setNombreRes(String nombreRes) {
+        this.nombreRes = nombreRes;
+    }
+
+    public Integer getFotoRes() {
+        return fotoRes;
+    }
+
+    public void setFotoRes(Integer fotoRes) {
+        this.fotoRes = fotoRes;
+    }
+
+    public String getRango() {
+        return rango;
+    }
+
+    public void setRango(String rango) {
+        this.rango = rango;
+    }
+
+    public String getTelefonoRes() {
+        return telefonoRes;
+    }
+
+    public void setTelefonoRes(String telefonoRes) {
+        this.telefonoRes = telefonoRes;
+    }
+
+    public String getRecomendado() {
+        return recomendado;
+    }
+
+    public void setRecomendado(String recomendado) {
+        this.recomendado = recomendado;
+    }
+}
