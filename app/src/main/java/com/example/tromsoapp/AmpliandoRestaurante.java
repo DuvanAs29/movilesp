ed0bc0f6aced614e5dbd974d2956b93775a82cb8
309 <309@academicos.cesde.edu.co>
2023-08-29 20:37:49 -0500

    ampliar

---
+++ app/src/main/java/com/example/tromsoapp/AmpliandoRestaurante.java
@@ -0,0 +1,13 @@
+package com.example.tromsoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AmpliandoRestaurante extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ampliando_restaurante);
+    }
+}
