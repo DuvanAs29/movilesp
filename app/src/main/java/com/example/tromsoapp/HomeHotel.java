3747da51cb91131d2fbf84bcef5e1fb77965b72b
309 <309@academicos.cesde.edu.co>
2023-09-20 20:53:26 -0500

    base datos

--- app/src/main/java/com/example/tromsoapp/HomeHotel.java
+++ app/src/main/java/com/example/tromsoapp/HomeHotel.java
@@ -1,13 +1,23 @@
 package com.example.tromsoapp;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.tromsoapp.adaptadores.AdaptadorHotel;
 import com.example.tromsoapp.moldes.MoldeHotel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
@@ -15,13 +25,33 @@ public class HomeHotel extends AppCompatActivity {
 
     ArrayList<MoldeHotel> lista_hotel=new ArrayList<>();
     RecyclerView recyclerView;
-
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_hotel);
         recyclerView=findViewById(R.id.dinamicahoteles);
         recyclerView.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL, false));
+        db.collection("hoteles")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+
+                                String nombreHotel= document.getString("nombre");
+                                String precioHotel= document.getString("precio");
+                                Toast.makeText(HomeHotel.this, nombreHotel, Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+
+                        }
+                    }
+                });
+
+
         llenarLIstaConDatos();
         AdaptadorHotel adaptadorHotel=new AdaptadorHotel(lista_hotel);
         recyclerView.setAdapter(adaptadorHotel);
